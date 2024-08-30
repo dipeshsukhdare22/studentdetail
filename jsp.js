2fdae1bb83f6d2580544ec328cfc650f3b9609d2
Dipesh Sukhdare <131474643+dipeshsukhdare22@users.noreply.github.com>
2024-08-30 21:57:42 +0530

    initial

---
+++ jsp.js
@@ -0,0 +1,21 @@
+function updateCard(){
+
+    document.getElementById("collegeNameVal").innerText = 
+    document.getElementById("collegeName").value;
+
+    document.getElementById("collegeNameVal").innerText = 
+    document.getElementById("collegeName").value;
+
+    document.getElementById("dateOfBirthVal").innerText = 
+    document.getElementById("dateOfBirth").value;
+
+    document.getElementById("classVal").innerText = 
+    document.getElementById("class").value;
+
+    document.getElementById("rollNumberVal").innerText = 
+    document.getElementById("rollNumber").value;
+
+    document.getElementById("contactNumberVal").innerText = 
+    document.getElementById("contactNumber").value;
+
+}
